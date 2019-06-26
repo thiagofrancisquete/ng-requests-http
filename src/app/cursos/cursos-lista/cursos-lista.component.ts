a330db022d0c821e38138089434997f0361fd0d6
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-06-26 16:15:50 -0300

    requests http

---
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Curso } from './../curso';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styleUrls: ['./cursos-lista.component.scss']
+})
+export class CursosListaComponent implements OnInit {
+
+  // cursos: Curso[];
+  cursos$: Observable<Curso[]>;
+
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    //this.service.list()
+    //.subscribe(dados => this.cursos = dados);
+    
+    this.cursos$ = this.service.list();
+  }
+
+}
